0ab68b1559129851017a3122b10760b0499bbb46
Behzad <be4zad@tuta.io>
2023-11-26 17:09:58 +0330

    Set fish window title

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,9 @@
 set -U fish_user_paths /usr/local/bin $fish_user_paths
 set -gx GPG_TTY (tty)
+
+# Sets window title
+function fish_title
+    set -q argv[1]; or set argv fish
+    set user (id -un)
+    echo $user@(prompt_hostname) - $argv[1]
+end
